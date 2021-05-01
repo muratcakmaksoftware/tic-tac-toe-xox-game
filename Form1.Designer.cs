4c309c26e68a78309cf8ea1218923cc5b3922dfb
Murat Çakmak <murat@seyum.com>
2021-05-01 16:19:43 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace xoxfrm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn1.Location = new System.Drawing.Point(72, 51);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(150, 150);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn2
+            // 
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn2.Location = new System.Drawing.Point(277, 51);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(150, 150);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn3.Location = new System.Drawing.Point(467, 51);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(150, 150);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn4
+            // 
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn4.Location = new System.Drawing.Point(72, 233);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(150, 150);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn5
+            // 
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn5.Location = new System.Drawing.Point(277, 233);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(150, 150);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn6
+            // 
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn6.Location = new System.Drawing.Point(467, 233);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(150, 150);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn7
+            // 
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn7.Location = new System.Drawing.Point(72, 414);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(150, 150);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn8.Location = new System.Drawing.Point(277, 414);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(150, 150);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // btn9
+            // 
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
+            this.btn9.Location = new System.Drawing.Point(467, 414);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(150, 150);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btnEvents);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 638);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "XOX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xoxfrm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rasgele = new Random();
+        string[,] btnisim = new string[3, 3]; // buton isimleri
+        string[,] tablo = new string[3, 3]; // oyun durumu        
+        string o;
+        int i = 0, d = 0;
+        bool drm = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Başlangıç rasgele oyunumuzu başlatalım.
+            i = 0; d = 0;
+            // buton isimleri diziye aktarılıyor.
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                {
+                    btnisim[i,d] = c.Name.ToString();
+                    d++;
+                }
+
+                if (d == 3)
+                {
+                    d = 0;
+                    i++;
+                }
+            }
+
+            // Form Açılış otomatik başlama
+            nextMove(); // Bilgisayarın oynucağı yeni adım            
+            tabloUpdate();
+            
+        }
+
+        private void nextMove()  // Bilgisayarın oynucağı yeni adım
+        {
+            bool drm = true;
+            while (drm)
+            {
+                int x = rasgele.Next(0, 3);
+                int y = rasgele.Next(0, 3);
+
+                if (tablo[x, y] == "X" || tablo[x, y] == "O") // işaretlenmiş yeri işaretlememek için.
+                    drm = true; // drm yanlış
+                else
+                {
+                    this.Controls.Find(btnisim[x, y], true)[0].Text = "O"; // kısa yöntem
+                    drm = false;
+                    #region uzun kod
+                    /*o = btnisim[x, y]; // buton ismi alınıyor.                    
+                    foreach (Control c in this.Controls) // tüm butonların isimleri kontrol ediliyor
+                    {
+                        if (c.GetType() == typeof(Button))
+                        {
+
+                            if (c.Name == o) // işaretlenecek buton
+                                c.Text = "O";
+                        }
+                    }*/
+                    #endregion
+                }
+
+            }
+            
+        }
+        
+        private string playCheck(string player) // Oyun Kontrolü
+        {
+            
+            string krk = "", drmply = "";
+            int dogrulama = 0, oyundurumu = 0;
+            // oyunun bitip bitmediğinin kontrolü
+            for (i = (tablo.Length / 3) - 1; i >= 0; i--)
+            {
+                krk = tablo[i, 0];
+                for (d = (tablo.Length / 3) - 1; d >= 0; d--)
+                {
+                    if (tablo[i, d] == "X" || tablo[i, d] == "O")
+                    {
+                        oyundurumu++;
+                    }
+                }
+            }
+            // tüm tablo doluysa oyun bitmiştir.
+            if (oyundurumu >= 9)
+            {
+                drmply = "Oyun Bitti"; // berabere kalındı durumu
+            }
+
+            //yatay kontrol
+            /*
+                str1 |0|1|2|
+                str2 | | | |
+                str3 | | | |
+            */
+            for (i = (tablo.Length / 3) - 1; i >= 0; i--)
+            {
+                for (d = (tablo.Length / 3) - 1; d >= 0; d--)
+                {
+                    if(tablo[i, d] == player) // "X"
+                    {
+                        dogrulama++;
+                    }
+                    if (dogrulama == 3)
+                    {
+                        drmply = "WON";
+                        break;
+                    }
+                }
+                dogrulama = 0;
+            }
+            
+            if (dogrulama != 3)
+                dogrulama = 0;
+
+            //dikey kontrol
+            /*
+                str1 |0| | |
+                str2 |1| | |
+                str3 |2| | |
+            */
+            for (i = (tablo.Length / 3) - 1; i >= 0; i--)
+            {
+                for (d = (tablo.Length / 3) - 1; d >= 0; d--)
+                {
+                    if (tablo[d, i] == player) 
+                    {
+                        dogrulama++;
+                    }
+                    if (dogrulama == 3)
+                    {
+                        drmply = "WON";
+                        break;
+                    }
+                }
+                dogrulama = 0;
+            }
+
+            if (dogrulama != 3)
+                dogrulama = 0;
+
+            //capraz sag dogru
+            d = 2;
+            for (i = (tablo.Length / 3) - 1; i >= 0; i--)
+            {
+                /*
+                str1 | | |2|
+                str2 | |1| |
+                str3 |0| | |
+                */
+                if (tablo[i, d] == player)
+                {                    
+                    dogrulama++;
+                }
+                if (dogrulama == 3)
+                {
+                    drmply = "WON";
+                    break;
+                }
+                d--;
+            }
+
+            if (dogrulama != 3)
+                dogrulama = 0;
+
+            //capraz sol dogru
+            d = 0;
+            for (i = (tablo.Length / 3) - 1; i >= 0; i--)
+            {
+                /*
+                str1 |0| | |
+                str2 | |1| |
+                str3 | | |2|
+                */
+
+                //MessageBox.Show(tablo[i, d]);
+                if (tablo[i, d] == player)
+                {
+                    dogrulama++;
+                }
+                if (dogrulama == 3)
+                {
+                    drmply = "WON";
+                    break;
+                }
+                d++;
+            }
+
+            
+
+            return drmply;           
+        }
+
+        private void tabloUpdate() // tablo güncellemesi
+        {
+            i = 0; d = 0;
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                {
+                    tablo[i, d] = c.Text.ToString();
+                    d++;
+                }
+
+                if (d == 3)
+                {
+                    d = 0;
+                    i++;
+                }
+            }
+        }
+
+        private void tabloYazdir() // test amaçlı tablo ekrana yazdırma
+        {
+
+            for (i = (tablo.Length / 3) - 1; i >= 0; i--)
+            {
+                for (d = (tablo.Length / 3) - 1; d >= 0; d--)
+                {
+                    MessageBox.Show(tablo[i, d]); 
+                }
+            }
+        }
+
+        private void oyunuSifirla() 
+        {
+            // butonların textini silme
+            foreach (Control c in this.Controls)
+                if (c.GetType() == typeof(Button))
+                    c.Text = "";
+
+            tablo = new string[3, 3];// tabloyu sıfırlama            
+
+            nextMove(); // bilgisayarı ilk oynasını yaptırma
+            tabloUpdate();
+
+            
+        }
+
+        string drmoyn = "";
+        private void btnEvents(object sender, EventArgs e) // tüm butonların tıklanma eventi
+        {
+
+            if (sender.GetType() == typeof(Button)) // Sadece buton tipindeki nesneler olduğunda if bloguna girer.
+            {
+                if (((Button)sender).Text == "") // butonun içinde yazı yoksa gir.
+                {
+                    drmoyn = ""; // oyun durumunu sıfırla
+                    ((Button)sender).Text = "X"; // benim oynadığım.
+                    tabloUpdate(); // tablo durumunu güncelle
+
+                    drmoyn = playCheck("X"); // ben oynadıktan sonra kontrol
+                    if (drmoyn == "WON")
+                    {
+                        MessageBox.Show("X Kazandı");
+                        oyunuSifirla();
+                        return;
+                    }
+                    
+                    // Bilgisayar oynuyor Yani "O"
+                    nextMove(); 
+                    tabloUpdate(); // tablo güncelleniyor
+
+                    drmoyn = playCheck("O");// Bilgisayar oynadıktan sonra kontrol.
+                    if (drmoyn == "WON")
+                    {
+                        MessageBox.Show("O Kazandı");
+                        oyunuSifirla();
+                    }
+                    else if (drmoyn == "Oyun Bitti")
+                    {
+                        MessageBox.Show("Berabere Kalındı.");
+                        oyunuSifirla();
+                    }
+
+                }
+                    
+            }
+        }
+    }
+}
